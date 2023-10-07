219e976992fd09ded857a030f4b614482275b69e
Lucas Carmona Neto <lucascarmonaneto510@gmail.com>
2023-10-07 12:32:42 -0300

    base front

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+$('.open_button').on('click', () => {
+    $('header').toggleClass('headerHidden');
+});
+
+$('.select').on('click', () => {
+    let $options = $('.options');
+    let $selectIcon = $('.select-icon');
+
+    if ($options.is(':visible')) {
+        $options.fadeOut('fast');
+        $selectIcon.css('transform', 'rotate(180deg)');
+    } else {
+        $options.fadeIn('fast');
+        $selectIcon.css('transform', 'rotate(0deg)');
+    }
+});
+
+$(document).on('click', (event) => {
+    let $options = $('.options');
+    let $selectIcon = $('.select-icon');
+
+    if (
+        !$options.is(event.target) &&
+        $options.has(event.target).length === 0 &&
+        !$('.select').is(event.target) &&
+        $('.select').has(event.target).length === 0
+    ) {
+        $options.fadeOut('fast');
+        $selectIcon.css('transform', 'rotate(180deg)');
+    }
+});
